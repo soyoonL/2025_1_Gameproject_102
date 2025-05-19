05bf62fabb7e680e7ef212d773889d008c8dae81
soyoonL <diamomo777@naver.com>
2025-05-19 16:21:20 +0900

    [feat]계급장 키우기 샘플 미완성

---
+++ Assets/Scripts/Game_LankMerge/DraggableRank.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableRank : MonoBehaviour
+{
+
+    public int rankLevel = 1;                   
+    public float dragSpeed = 10f;               
+    public float snapBackSpeed = 20f;           
+
+    public bool isDragging = false;             
+    public Vector3 originalPosition;            
+    public GridCell currentCell;                
+
+    public Camera mainCamera;                   
+    public Vector3 dragOffset;                  
+    public SpriteRenderer spriteRenderer;       
+    public GameManager gamemanager;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        gamemanager = GetComponent<GameManager>();
+    }
+
+
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    void StartDragging()
+    {
+        isDragging = true;
+        dragOffset = transform.position-GetMouseWorldPosition();
+        spriteRenderer.sortingOrder = 10;
+    }
+
+    void StopDragging()
+    {
+        isDragging = false;
+        spriteRenderer.sortingOrder = 0;
+    }
+
+
+    public void MoveToCell(GridCell targetCell)     
+    {
+        if (currentCell != null)
+        {
+            currentCell.currentRank = null; 
+        }
+
+        currentCell = targetCell;           
+        targetCell.currentRank = this;
+
+        originalPosition = new Vector3(targetCell.transform.position.x, targetCell.transform.position.y, 0f);
+        transform.position = originalPosition;
+    }
+
+    public void ReturnToOriginalPosition()
+    {
+        transform.position = originalPosition;
+    }
+
+    public void MergeWithCell(GridCell targetCell)
+    {
+        if (targetCell.currentRank == null || targetCell.currentRank.rankLevel != rankLevel) 
+        {
+            ReturnToOriginalPosition(); 
+            return;
+        }
+
+        if (currentCell != null)
+        {
+            currentCell.currentRank = null; 
+        }
+
+    }
+
+    public Vector3 GetMouseWorldPosition()                  
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -mainCamera.transform.position.z;
+        return mainCamera.ScreenToWorldPoint(mousePos);
+    }
+
+    public void SetRankLevel(int level)
+    {
+        rankLevel = level;
+
+        if (gamemanager != null && gamemanager.rankSprites.Length > level - 1)
+        {
+            spriteRenderer.sprite = gamemanager.rankSprites[level - 1];    
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Game_LankMerge/GameManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int gridWidth = 7;
+    public int gridHeight = 7;
+    public float cellSize = 1.4f;
+    public GameObject cellPrefabs;
+    public Transform gridContainer;
+    public GameObject rankPrefabs;
+    public Sprite[] rankSprites;
+    public int maxRankLevel = 7;
+    public GridCell[,] grid;
+    void InitializeGrid()
+    {
+        grid = new GridCell[gridWidth, gridHeight];
+
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                Vector3 position = new Vector3(
+                    x * cellSize - (gridWidth * cellSize / 2) + cellSize / 2,
+                    y * cellSize - (gridHeight * cellSize / 2) + cellSize / 2,
+                    1f
+                );
+
+                GameObject cellObj = Instantiate(cellPrefabs, position, Quaternion.identity, gridContainer);
+                GridCell cell = cellObj.AddComponent<GridCell>();
+                cell.Initialize(x, y);
+
+                grid[x, y] = cell;
+            }
+        }
+    }
+
+
+    void Start()
+    {
+        InitializeGrid();
+
+        for (int i = 0; i < 4; i++)
+        {
+            SpawnNewRank();
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public DraggableRank CreateRankInCell(GridCell cell, int level)
+    {
+
+        if (cell == null || !cell.IsEmpty()) return null;
+
+        level = Mathf.Clamp(level, 1, maxRankLevel);
+
+        Vector3 rankPosition = new Vector3(cell.transform.position.x, cell.transform.position.y, 0f);
+
+        GameObject rankObj = Instantiate(rankPrefabs, rankPosition, Quaternion.identity, gridContainer);
+        rankObj.name = "Rank_Lvel" + level;
+
+        DraggableRank rank = rankObj.AddComponent<DraggableRank>();
+        rank.SetRankLevel(level);
+
+        return rank;
+
+    }
+
+    private GridCell FindEmptyCell()
+    {
+        List<GridCell> emptyCells = new List<GridCell>();
+
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                if (grid[x, y].IsEmpty())
+                {
+                    emptyCells.Add(grid[x, y]);
+                }
+            }
+        }
+
+        if (emptyCells.Count == 0)
+        {
+            return null;
+        }
+
+        return emptyCells[Random.Range(0, emptyCells.Count)];
+
+    }
+
+    public bool SpawnNewRank()
+    {
+        GridCell emptyCell = FindEmptyCell();
+        if (emptyCell == null) return false;
+
+        int rankLevel = Random.Range(0, 100) < 80 ? 1 : 2;
+        CreateRankInCell(emptyCell, rankLevel);
+
+        return true;
+    }
+
+    public GridCell FindClosestCell(Vector3 position)
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                if (grid[x, y].ContainsPosition(position))
+                {
+                    return grid[x, y];
+                }
+            }
+        }
+        GridCell closestCell = null;
+        float closestDistance = float.MaxValue;
+
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0;y < gridHeight; y++)
+            {
+                float distance = Vector3.Distance(position, grid[x, y], transform.position);
+                if (distance < closestDistance)
+                {
+                    
+                }
+            }
+        }
+    }
+}
